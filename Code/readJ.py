40f4b421f205a8306864568eff89709ec1798ce3
abhigyan <abhigyandeepbose@gmail.com>
2018-06-04 09:06:23 +0530

    CSV by Id Created

--- Code/readJ.py
+++ Code/readJ.py
@@ -1,4 +1,5 @@
 import json
+import csv
 import os
 
 main_directory = '../Datasets/phemernrdataset/pheme-rnr-dataset'
@@ -16,24 +17,28 @@ s_r = ['source-tweet', 'reactions']
 
 
 path_rnr = main_directory+'/'+events[0]+'/'+r_nr[0]
+csvFile = open('charliehebdoTweetGraph(Id).csv', 'w', newline='')
+csvWriter = csv.writer(csvFile)
 for idx1, t_name in enumerate(os.listdir(os.getcwd()+'/'+path_rnr)):
     path_t = path_rnr + '/' + t_name
     print(t_name)
     filename = os.listdir(os.getcwd()+'/'+path_t+'/'+s_r[0])[0]
+    source_id = ''
     with open(
             path_t + '/' + s_r[0] + '/' + filename) as f:
         j_data = json.load(f)
         # print(j_data)
-        user_data = j_data['user']['id']
-        print(user_data)
-    #print(filename)
+        source_id = j_data['user']['id']
+        print(source_id)
+    # print(filename)
     for idx2, filename in enumerate(os.listdir(os.getcwd()+'/'+path_t+'/'+s_r[1])):
         # print(str(idx2 + 1) + ': ' + filename)
         with open(
                 path_t + '/' + s_r[1] + '/' + filename) as f:
             j_data = json.load(f)
             # print(j_data)
-            user_data = j_data['user']['id']
-            print(str(idx2+1) + ': ' + str(user_data))
+            response_id = j_data['user']['id']
+            print(str(idx2+1) + ': ' + str(response_id))
+            csvWriter.writerow([source_id, response_id, '1'])
 
-
+csvFile.close()
